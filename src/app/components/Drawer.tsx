b4db40ab7ba763b4d3d91303361b3052f15963c5
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-03-09 12:38:47 -0500

    Add @headlessui/react dependency and update page layout

---
+++ src/app/components/Drawer.tsx
@@ -0,0 +1,107 @@
+'use client'
+
+import { useState } from 'react'
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle, TransitionChild } from '@headlessui/react'
+import { IoCloseOutline } from "react-icons/io5";
+import drawerStyles from './styles/drawer.module.css';
+
+interface Props {
+  open: boolean;
+  setOpen: (open: boolean) => void
+}
+
+export default function Drawer({ open, setOpen }: Props) {
+  // const [open, setOpen] = useState(false)
+
+  return (
+    <Dialog open={open} onClose={setOpen} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500/75 transition-opacity duration-500 ease-in-out data-closed:opacity-0"
+      />
+      <div className="fixed inset-0 overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <DialogPanel
+              transition
+              className="pointer-events-auto relative w-70 max-w-md transform transition duration-500 ease-in-out data-closed:translate-x-full sm:duration-700"
+            >
+              <TransitionChild>
+                <div id='close-button-container' className="absolute top-0 left-0 -ml-8 flex pt-4 pr-2 duration-500 ease-in-out data-closed:opacity-0 sm:-ml-10 sm:pr-4">
+                  <button
+                    type="button"
+                    onClick={() => setOpen(false)}
+                    className="relative rounded-md text-gray-300 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden"
+                  >
+                    <IoCloseOutline />
+                    {/* <XMarkIcon aria-hidden="true" className="size-6" /> */}
+                  </button>
+                </div>
+              </TransitionChild>
+              <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                <div className="px-4 sm:px-6">
+                  <DialogTitle className="text-base font-semibold text-gray-900 mt-6">Menu</DialogTitle>
+                </div>
+                <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                  <nav className="navbar">
+                    <ul className="navbarList flex flex-col gap-5 text-black items-center">
+                      <li id="itemEnlace" className="navbarListItem">
+                        <a href="/" className="navbarListItemLink" id="inicio"
+                        >Inicio</a
+                        >
+                      </li>
+                      <li id="itemEnlace" className="navbarListItem">
+                        <a
+                          href="#welcome"
+                          className="navbarListItemLink"
+                          id="welcome"
+                        >Welcome</a
+                        >
+                      </li>
+                      <li id="itemEnlace" className="navbarListItem">
+                        <a
+                          href="#aboutMe"
+                          className="navbarListItemLink"
+                          id="aboutMe"
+                        >About me</a
+                        >
+                      </li>
+                      <li id="itemEnlace" className="navbarListItem">
+                        <a
+                          href="#skills"
+                          className="navbarListItemLink"
+                          id="skills"
+                        >Skills</a
+                        >
+                      </li>
+                      <li id="itemEnlace" className="navbarListItem">
+                        <a
+                          href="#projects"
+                          className="navbarListItemLink"
+                          id="projects"
+                        >Projects</a
+                        >
+                      </li>
+                      <li id="itemEnlace" className="navbarListItem">
+                        <a
+                          href="#contactMe"
+                          className="navbarListItemLink"
+                          id="contactForm"
+                        >Contact Form</a
+                        >
+                      </li>
+                    </ul>
+                    <select id="languageSelector">
+                      <option value="es">Español</option>
+                      <option value="en">English</option>
+                    </select>
+                  </nav>
+                </div>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  )
+}
