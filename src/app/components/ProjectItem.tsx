82f587df6a884615429ca9b8314aad4c88abbc35
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-03-16 08:06:55 -0500

    Refactor ProjectItem component styling

--- src/app/components/ProjectItem.tsx
+++ src/app/components/ProjectItem.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import proojectStyles from './styles/projectItem.module.css';
 import Image from 'next/image';
 
 export const ProjectItem = () => {
   return (
-    <div className={`rounded-xl p-5 bg-[#191b1f] grid gap-3 place-items-center`}>
-      <Image className='w-40 h-40 rounded-xl object-cover object-center' src="/ptll_ppal_app_mhwebp.webp" alt="project image" width={100} height={100} />
-      <section className="projectInfo p-4 bg-[#25272a]">
+    <div className={`rounded-xl p-5 bg-[#191b1f] hover:bg-black hover:border hover:border-gray-700 grid gap-3 place-items-center`}>
+      <Image className='w-40 h-50 rounded-xl object-cover object-center' src="/ptll_ppal_app_mhwebp.webp" alt="project image" width={100} height={100} />
+      <section className="projectInfo p-4 bg-[#25272a] hover:bg-black rounded-lg">
         <h4 className="projectInfoTitle text-lg font-semibold w-auto">App Mario Hernandez</h4>
         <p className="description mt-4">
           Esta es una aplicación híbrida que utiliza una base desarrollada con React Native. Embebe un sitio completo construido en VTEX, junto con componentes personalizados en React.js, facilitando la compra de una variedad de productos de la marca.
@@ -17,11 +16,13 @@ export const ProjectItem = () => {
         </div>
         <div className="technologiesContainer">
           <span className='font-semibold'>Tecnologias: </span>
-          <div>
-            
+          <div className='flex gap-2'>
+         
+
           </div>
         </div>
       </section>
     </div>
   )
 }
+
