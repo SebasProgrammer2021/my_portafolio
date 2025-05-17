caf40aff09fa8202b6a81d98a7a22e96f15c0c49
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-05-17 12:47:58 -0500

    impelmentacion datos para el contact, y formateo

---
+++ src/app/contact/data.tsx
@@ -0,0 +1,25 @@
+import { SiLinkedin } from 'react-icons/si';
+import { SiInstagram } from 'react-icons/si';
+import { RxGithubLogo } from "react-icons/rx";
+
+
+export const contactData = [
+  {
+    name: 'Github',
+    username: 'SebasProgrammer2021',
+    icon: <RxGithubLogo size={30} />,
+    link: 'https://github.com/SebasProgrammer2021/'
+  },
+  {
+    name: 'Linkedin',
+    username: 'Sebastián Londoño Valencia',
+    icon: <SiLinkedin size={30} />,
+    link: 'https://www.linkedin.com/in/sebasti%C3%A1n-londo%C3%B1o-valencia/'
+  },
+  {
+    name: 'Instagram',
+    username: '@sebas_val_ing',
+    icon: <SiInstagram size={30} />,
+    link: 'https://www.instagram.com/sebas_val_ing/'
+  }
+];
