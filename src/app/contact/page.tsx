ad73ea9c1fd7164415bf93aca76020fe952916d2
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-05-18 10:52:39 -0500

    pagina contacto: se temrinan ajsutes para las diferentes tamaños de pantallla

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,14 +4,14 @@ import { contactData } from './data';
 
 const ContactPage = () => {
   return (
-    <div className='p-6 flex flex-col gap-10 mt-2'>
+    <div className='p-6 flex flex-col md:flex-row gap-10 mt-2 md:items-center md:justify-center h-screen'>
       {contactData.map((contact) => (
         <Link key={contact.name} href={contact.link}>
-          <div className='flex flex-col gap-6 items-center border rounded-xl border-white p-6'>
-            <picture>
+          <div className='flex flex-col gap-6 items-center border rounded-xl border-gray-400 p-6 md:py-24 md:max-w-64 md:max-h-96 lg:max-w-full'>
+            <picture className='border border-gray-400 p-5 rounded-full'>
               {contact.icon}
             </picture>
-            <span className='text-xl text-center font-bold'>
+            <span className='text-xl text-center font-bold md:mt-5 md:px-4'>
               {contact.username}
             </span>
             <span className='text-lg text-center'>
