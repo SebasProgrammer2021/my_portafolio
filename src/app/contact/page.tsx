c441a3ada098845b081be0e8e86e084876d536f6
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-05-17 20:06:59 -0500

    implementacion pagina contacto

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,25 @@
 import Link from 'next/link';
 import React from 'react'
+import { contactData } from './data';
 
 const ContactPage = () => {
   return (
-    <div className=''>
-      <span className='text-2xl block'>ContactPage</span>
-      <Link href="/">home</Link>
+    <div className='p-6 flex flex-col gap-10 mt-2'>
+      {contactData.map((contact) => (
+        <Link key={contact.name} href={contact.link}>
+          <div className='flex flex-col gap-6 items-center border rounded-xl border-white p-6'>
+            <picture>
+              {contact.icon}
+            </picture>
+            <span className='text-xl text-center font-bold'>
+              {contact.username}
+            </span>
+            <span className='text-lg text-center'>
+              {contact.name}
+            </span>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
