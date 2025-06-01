edda877cdf7bfeae524d59263320be04660a42e2
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-05-31 21:48:37 -0500

    stye: ajuste hover

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = ({ setOpen }: INavbarProps) => {
     <nav className="navbar dark:bg-neutral-900 lg:w-fit rounded-3xl">
       <ul className="navbarList flex flex-col gap-5 lg:gap-0 text-black items-center text-xl lg:flex-row lg:w-full lg:justify-center">
         {navigation.map((item) => (
-          <li key={item.name} id="itemEnlace" className="navbarListItem flex items-center lg:hover:bg-gray-300 hover:rounded-3xl text-black dark:text-white font-semibold">
+          <li key={item.name} id="itemEnlace" className="navbarListItem flex items-center hover:bg-gray-300 dark:hover:text-black hover:rounded-3xl text-black dark:text-white font-semibold">
             <Link href={item.href} className="navbarListItemLink lg:p-3 " onClick={() => setOpen && setOpen(false)}>{item.name}</Link>
           </li>
         ))}
