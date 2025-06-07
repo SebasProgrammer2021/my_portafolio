a8d056aa7b4f47cdbb58d5b4520d14813e8902ac
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-06-07 14:21:21 -0500

    fix: eliminacion import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Aboutme } from "./components/Aboutme"
-import { Header } from "./components/Header"
 import { Projects } from "./components/Projects"
 import { Skills } from "./components/Skills"
 import { Welcome } from "./components/Welcome"
